f435ab63e483280f46226bd91f7c6a224d262b74
CN workstation <cn-workstation@autox.ai>
2019-05-16 00:40:02 +0800

    only reserve std branch2

--- src/test_hello.cc
+++ src/test_hello.cc
@@ -1,10 +1,6 @@
 #include <iostream>
 
 int main(){
-std::cout<<"Hello Kun SUN1"<<std::endl;
- std::cout<<"Hello Kun SUN3"<<std::endl;
-std::cout<<"Hello Kun SUN4"<<std::endl;
-std::cout<<"Hello Kun SUN5"<<std::endl;
 std::cout<<"Hello Brach2"<< std::endl;
 
 }
