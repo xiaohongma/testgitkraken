4187f32e64d2f70348029b41e9e6a507207d687c
CN workstation <cn-workstation@autox.ai>
2019-05-16 00:38:39 +0800

    stdout branch2

--- src/test_hello.cc
+++ src/test_hello.cc
@@ -5,5 +5,6 @@ std::cout<<"Hello Kun SUN1"<<std::endl;
  std::cout<<"Hello Kun SUN3"<<std::endl;
 std::cout<<"Hello Kun SUN4"<<std::endl;
 std::cout<<"Hello Kun SUN5"<<std::endl;
+std::cout<<"Hello Brach2"<< std::endl;
 
 }
