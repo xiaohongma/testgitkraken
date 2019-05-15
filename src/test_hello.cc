cde6d1dbd8925103a36058c06356cc5c924c7c83
CN workstation <cn-workstation@autox.ai>
2019-05-15 20:41:09 +0800

    solve annotation

--- src/test_hello.cc
+++ src/test_hello.cc
@@ -1,14 +1,14 @@
 #include <iostream>
 
 int main(){
-<<<<<<< HEAD
+//<<<<<<< HEAD
  std::cout<<"Hello Kun SUN2"<<std::endl;
  std::cout<<"Hello Kun SUN21 master"<<std::endl;
-=======
+//=======
 std::cout<<"Hello Kun SUN1"<<std::endl;
  std::cout<<"Hello Kun SUN3"<<std::endl;
 std::cout<<"Hello Kun SUN4"<<std::endl;
 std::cout<<"Hello Kun SUN5"<<std::endl;
->>>>>>> branch1
+//>>>>>>> branch1
 
 }
